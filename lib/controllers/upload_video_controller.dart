e7d3ea90d086d10ffb34d14a092e4bf6491f92e8
unknown <hybiekay2@gmail.com>
2025-07-14 22:05:46 +0100

    first commit

---
+++ lib/controllers/upload_video_controller.dart
@@ -0,0 +1,123 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloudinary_api/uploader/cloudinary_uploader.dart';
+import 'package:cloudinary_api/uploader/uploader_utils.dart';
+import 'package:cloudinary_api/src/request/model/uploader_params.dart';
+import 'package:cloudinary_url_gen/cloudinary.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone/models/video.dart';
+import 'package:uuid/uuid.dart';
+import 'package:video_compress/video_compress.dart';
+
+class UploadVideoController extends GetxController {
+  RxString status = "".obs;
+  var cloudinary = Cloudinary.fromStringUrl(
+    'cloudinary://132994723556147:aVSUhDoUYleignd1YjqvyQAmrLg@dtn3mnyya',
+  );
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<File> _compressVideo(String videoPath) async {
+    final compressedVideo = await VideoCompress.compressVideo(
+      videoPath,
+      quality: VideoQuality.MediumQuality,
+    );
+    return compressedVideo!.file!;
+  }
+
+  Future<File> _getThumbnail(String videoPath) async {
+    final thumbnail = await VideoCompress.getFileThumbnail(videoPath);
+    return thumbnail;
+  }
+
+  Future<String?> _uploadVideoToCloudinary(File file) async {
+    try {
+      Map options = {};
+      var response = await cloudinary.uploader().upload(
+        file,
+
+        progressCallback: (val, le) {
+          print("the $val, and the $le");
+        },
+        params: UploadParams(resourceType: 'video'),
+      );
+      print('Cloudinary response: ${response?.data?.url}');
+      return response?.data?.url;
+    } catch (e) {
+      print('Cloudinary upload error: $e');
+      return null;
+    }
+  }
+
+  Future<String?> _uploadFileToCloudinary(File file) async {
+    try {
+      var response = await cloudinary.uploader().upload(
+        file,
+
+        progressCallback: (val, le) {
+          print("the $val, and the $le");
+        },
+      );
+      print('Cloudinary response: ${response?.data?.url}');
+      return response?.data?.url;
+    } catch (e) {
+      print('Cloudinary upload error: $e');
+      return null;
+    }
+  }
+
+  Future<void> uploadVideo(
+    String songName,
+    String caption,
+    String videoPath,
+    File videoFile,
+  ) async {
+    try {
+      String uid = _auth.currentUser!.uid;
+      String videoId = Uuid().v4();
+      DocumentSnapshot userDoc =
+          await _firestore.collection('users').doc(uid).get();
+      // var allDocs = await _firestore.collection('videos').get();
+      // int len = allDocs.docs.length;
+      // // String videoId = "Video $len";
+      File compressedVideo = await _compressVideo(videoPath);
+      String? videoUrl = await _uploadVideoToCloudinary(compressedVideo);
+      File thumbnailFile = await _getThumbnail(videoPath);
+      String? thumbnailUrl = await _uploadFileToCloudinary(thumbnailFile);
+
+      if (videoUrl == null || thumbnailUrl == null) {
+        Get.snackbar('Error', 'Cloudinary upload failed');
+        return;
+      }
+
+      Video video = Video(
+        username: (userDoc.data()! as Map<String, dynamic>)['name'],
+        uid: uid,
+        id: videoId,
+        likes: [],
+        commentCount: 0,
+        shareCount: 0,
+        songName: songName,
+        caption: caption,
+        videoUrl: videoUrl,
+        profilePhoto: (userDoc.data()! as Map<String, dynamic>)['profilePhoto'],
+        thumbnail: thumbnailUrl,
+      );
+
+      print('Saving video metadata to Firestore...');
+      await _firestore.collection('videos').doc(videoId).set(video.toJson());
+      print('Saved video metadata to Firestore!');
+      Get.back();
+      Get.snackbar(
+        'Success',
+        'Video uploaded to Cloudinary and saved in Firebase!',
+      );
+    } catch (e) {
+      print('Error saving video: $e');
+      Get.snackbar('Error Uploading Video', e.toString());
+    }
+  }
+}
---
+++ lib/views/screens/add_video_screen.dart
@@ -0,0 +1,104 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tiktok_clone/constants.dart';
+import 'package:tiktok_clone/views/screens/confirm_screen.dart';
+
+class AddVideoScreen extends StatelessWidget {
+  const AddVideoScreen({super.key});
+
+  pickVideo(ImageSource src, BuildContext context) async {
+    print("I want to file file");
+    final video = await ImagePicker().pickVideo(source: src);
+    print("am done piking file");
+
+    if (video != null) {
+      print("file is not null");
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder:
+              (context) => ConfirmScreen(
+                videoFile: File(video.path),
+                videoPath: video.path,
+              ),
+        ),
+      );
+    } else {
+      print("you pick nooting");
+    }
+  }
+
+  showOptionsDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder:
+          (context) => SimpleDialog(
+            children: [
+              SimpleDialogOption(
+                onPressed: () => pickVideo(ImageSource.gallery, context),
+                child: Row(
+                  children: const [
+                    Icon(Icons.image),
+                    Padding(
+                      padding: EdgeInsets.all(7.0),
+                      child: Text('Gallery', style: TextStyle(fontSize: 20)),
+                    ),
+                  ],
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () => pickVideo(ImageSource.camera, context),
+                child: Row(
+                  children: const [
+                    Icon(Icons.camera_alt),
+                    Padding(
+                      padding: EdgeInsets.all(7.0),
+                      child: Text('Camera', style: TextStyle(fontSize: 20)),
+                    ),
+                  ],
+                ),
+              ),
+              SimpleDialogOption(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Row(
+                  children: const [
+                    Icon(Icons.cancel),
+                    Padding(
+                      padding: EdgeInsets.all(7.0),
+                      child: Text('Cancel', style: TextStyle(fontSize: 20)),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: InkWell(
+          onTap: () => showOptionsDialog(context),
+          child: Container(
+            width: 190,
+            height: 50,
+            decoration: BoxDecoration(color: buttonColor),
+            child: const Center(
+              child: Text(
+                'Add Video',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
